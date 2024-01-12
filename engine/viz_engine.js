a29466086ecda244c0532b4bba21bdf173561bd1
Bokeh-ascii <149326813+Bokeh-ascii@users.noreply.github.com>
2024-01-12 20:49:54 +0100

    millores code

--- engine/viz_engine.js
+++ engine/viz_engine.js
@@ -75,8 +75,17 @@ class Graph{
      *  @returns svg height
      */
     get_height(){
-        return this.viewboox_height - (this.margin_top + this.margin_left);
+        return this.viewboox_height - (this.margin_top + this.margin_botton);
     }
+
+    get_x(){
+        return this.margin_left;
+    }
+
+    get_y(){
+        return this.margin_top;
+    }
+
     /**
      *  @returns svg area
      */
@@ -94,9 +103,9 @@ class Graph{
      * 
      * @returns string with svg to draw a rect for waffle 
      */
-        rect_svg(x, y, size, css_class, r=0){
-            return '<rect class="' + css_class + '" x="' + x + '" y="' + y +'" width="' + size +'" height="' + size +'" rx="1" ry="1" />'
-        }
+    rect_svg(x, y, size, css_class, r=0){
+        return '<rect class="' + css_class + '" x="' + x + '" y="' + y +'" width="' + size +'" height="' + size +'" rx="1" ry="1" />'
+    }
 
 
 }
@@ -124,6 +133,8 @@ class WaffleChart extends Graph{
         this.margin_left = 100;
 
         this.rects_margin = 2;
+
+        this.r = 10;
     }
     /**
      * @returns total squares of the chart
@@ -141,21 +152,26 @@ class WaffleChart extends Graph{
 
  
     get_html(viewboox_width = 800, viewboox_height=800){
-        let html = this.get_svg_start_tag();
+        return this.get_svg_start_tag() + this.get_html_rects() + this.get_svg_end_tag();
+    }
 
+    get_html_rects(){
+        let html = '';
         let size = this.get_rect_size(this.get_chart_size())
-
         for(let i=0; i < this.get_squares(); i++){
-            let class_style = 'main';
-            if( this.is_highlighted(i)){
-                class_style = 'highlighted';
-            }
             html += this.rect_svg(
                 this.get_rect_x(i,size), this.get_rect_y(i, size),
-                size, class_style, r = 10
-            )
+                size, this.get_rect_class(), this.r
+                )
         }
-        return html + this.get_svg_end_tag();
+        return html;
+    }
+
+    get_rect_class(i){
+        if( this.is_highlighted(i)){
+            return 'highlighted';
+        }
+        return 'main'
     }
 
     /**
@@ -219,22 +235,94 @@ class WaffleChart extends Graph{
 }
 
 /***
- * Defines a unit formed by a key and a value
+ * Defines a node for hierarchical graphs
  */
-class KeyValue{
-    constructor(key, value){
-        this.key = key;
+class TreeMapNode{
+    constructor(node, value = 0){
+        this.code = node;
         this.value = value;
-        this.area = null;
+
+        this.supernode = null;
+
+        this.x = null;
+        this.y = null;
+        this.width = null;
+        this.height = null;
+    }
+
+    get_value(){
+        return this.value;
+    }
+
+    get_x(){
+        return this.x;
+    }
+
+    get_y(){
+        return this.y;
+    }
+
+    get_width(){
+        return this.width;
+    }
+
+    get_height(){
+        return this.height;
+    }
+
+    get_area(){
+        return this.width * this.height;
+    }
+
+    set_value(value){
+        this.value = value;
+    }
+
+    set_x(x){
+        this.x = x;
+    }
+
+    set_y(y){
+        this.y = y;
+    }
+
+    set_width(width){
+        this.width = width;
+    }
+
+    set_height(height){
+        this.height = height;
+    }
+
+    set_supernode(supernode){
+        this.supernode = supernode;
+
+        this.update_supernode_value();
+    }
+
+    update_supernode_value(){
+        let supernode = this.supernode;
+        while(supernode){
+            supernode.set_value(
+                supernode.get_value() + this.value
+                )
+            supernode = supernode.supernode;
+        }
     }
 }
 
-class KeyValueArray{
-    constructor(){
-        this.list = []
+class TreeMapSuperNode extends TreeMapNode{
+    constructor(node, value = 0){
+        super(node, value);
+        this.list = [];
+        this.type = 'portait';
     }
-    push(keyvalue){
-        this.list.push(keyvalue);
+    push(node){
+        this.list.push(node);
+
+        node.set_supernode(this);
+
+        this.update_nodes_parameters();
     }
     sort(){
         for (let i = 0; i < this.list.length; i++) {
@@ -249,11 +337,11 @@ class KeyValueArray{
     }
 
     reverse(){
-        let list_ = [];
-        for(let i = this.list.length - 1; i <= 0; i--){
-            list_.push(this.list[i]);
+        let list = [];
+        for(let i = this.list.length - 1; i >= 0; i--){
+            list.push(this.list[i]);
         }
-        this.list = list_;
+        this.list = list;
     }
 
     reverse_sort(){
@@ -261,27 +349,75 @@ class KeyValueArray{
         this.reverse();
     }
 
-    /**
-     * 
-     * @returns the total sum of values
-     */
-    get_sum(){
-        let sum = 0;
-        for(let i=0; i < this.list.length; i++){
-            sum += parseInt(this.list[i].value);
+    node_proportion(node){
+        if(this.value == 0){
+            return 1;
         }
-        return sum;
+        return node.get_value() / this.value;
     }
 
-    length(){
-        return this.list.length;
+    set_infranode_width(node){
+        let width = this.width;
+        if(this.type == 'portait'){
+            width *= this.node_proportion(node); 
+        }
+        node.set_width(width);
     }
 
-    set_area(i, area){
-        this.list[i].area = area;
+    set_infranode_height(node){
+        let height = this.height;
+        if(this.type == 'landscape'){
+            height *= this.node_proportion(node);
+        }
+        node.set_height(height);
     }
-    get_value(i){
-        return this.list[i].value;
+
+    set_infranode_x(node){
+        let x = this.x;
+        if(this.type == 'portait'){
+            let i = 0;
+            while(this.list[i] != node){
+    
+                x += node.get_width();
+                i++;
+            }
+        }
+        node.set_x(x);
+    }
+
+    set_infranode_y(node){
+        let y = this.y;
+        if(this.type == 'landscape'){
+            let i = 0;
+            while(this.list[i] != node){
+                y += node.get_height();
+                i++;
+            }
+        }
+        node.set_y(y);
+    }
+
+    update_nodes_parameters(){
+        this.reverse_sort();
+
+        for(let i = 0; i < this.list.length; i++){
+            let node = this.list[i];
+
+            this.set_infranode_width(node);
+            this.set_infranode_height(node);
+
+            this.set_infranode_x(node);
+            this.set_infranode_y(node);
+        }
+    }
+
+    set_type(type){
+        if(type == 'landscape'){
+            this.type = 'landscape';
+        }else{
+            this.type = 'portait';
+        }
+        this.update_nodes_parameters();
     }
 }
 
@@ -289,31 +425,132 @@ class KeyValueArray{
  * 
  */
 class TreeMap extends Graph{
-    constructor(id, keyvaluearray){
+    constructor(id){
         super(id);
-        this.list = keyvaluearray;
+        this.supernodes = new TreeMapSuperNode('');
 
-        this.update_areas();
+        this.set_supernodes_parameter();
+
     }
 
-    /**
-     * 
-     * @returns an array with the area must have each item
-     */
-    update_areas(){
-        for(let i=0; i < this.list.length(); i++){
-            this.list.set_area(
-                i, this.list.get_value(i) * this.get_area() / this.list.get_sum()
-            );
+    set_supernodes_parameter(){
+        this.supernodes.set_width(this.get_width());
+        this.supernodes.set_height(this.get_height());
+
+        this.supernodes.set_x(this.get_x());
+        this.supernodes.set_y(this.get_y());
+    }
+
+    push(node){
+        this.supernodes.push(node);
+    }
+
+    
+}
+
+/***
+ *
+ */
+class IconGraph extends Graph{
+    constructor(id, icon, labels, values){
+        super(id);
+        this.icon = icon;
+        this.labels = labels;
+        this.values = values;
+
+        this.graph_values = [];
+        this.update_graph_values()
+    }
+    sort(){
+        sort_parallel_lists(this.values, this.labels);
+    }
+    reverse(){
+        this.labels.reverse();
+        this.values.reverse();
+    }
+    inverse_sort(){
+        this.sort();
+        this.reverse();
+    }
+
+    update_graph_values(){
+        this.inverse_sort();
+        if(this.labels.length > 4){
+            let others_tag = 'others'
+            this.graph_values.push(
+                [this.labels[0], this.values[0]]
+            )
+            this.graph_values.push(
+                [this.labels[1], this.values[1]]
+            )
+            this.graph_values.push(
+                [this.labels[2], this.values[2]]
+            )
+            let others_sum = 0;
+            for(let i=3; i < this.values.length; i++){
+                others_sum += this.values[i];
+            }
+            this.graph_values.push(
+                [others_tag, others_sum]
+            )
+        }else{
+            for(i=o; i < this.values.length; i++){
+                this.graph_values.push(
+                    [this.labels[i], this.values[i]]
+                )
+            }
         }
     }
 
-    get_html(){
-        this.list.reverse_sort();
+    draw_html(){
 
-        let big_area1 = this.get_areas()[0];
-        let big_area2 = this.get_areas()[1] + his.get_areas()[2];
-        let big_area3 = this.get_area() - (big_area1 + big_area2);
+    }
+}
+
+class Icon{
+    constructor(svg_code){
+        this.svg_code = svg_code;
+
+        this.viexbox_width = null;
+        this.viewboox_height = null;
+
+        this.obtain_viewbox_values();
+    }
+
+    obtain_viewbox_values(){
+        const viewBoxIndex = this.svg_code.indexOf('viewBox');
+
+        const viewBoxSubstring = this.svg_code.substring(viewBoxIndex);
+
+        this.viexbox_width = viewBoxSubstring;
+
+        const firstOpenComas = viewBoxSubstring.indexOf('"');
+        const firstCloseComas = viewBoxSubstring.indexOf('"', firstOpenComas + 1);
+
+        const viewBoxValuesSubstring = viewBoxSubstring.substring(firstOpenComas + 1, firstCloseComas);
+
+
+
+        const values = viewBoxValuesSubstring.split(' ')
+
+        this.viexbox_width = parseFloat(values[2]);
+        this.viexbox_height = parseFloat(values[3]);
+    }
+
+}
+
+function sort_parallel_lists(list1, list2){
+    for (let i = 0; i < list1.length; i++) {
+        for (let j = 0; j < list1.length - i - 1; j++) {
+          if (list1[j] > list1[j + 1]) {
+            const lesser1 = list1[j + 1];
+            const lesser2 = list2[j+1];
+            list1[j + 1] = list1[j];
+            list2[j + 1] = list2[j];
+            list1[j] = lesser1;
+            list2[j] = lesser2;
+          }
+        }
     }
 }
 
